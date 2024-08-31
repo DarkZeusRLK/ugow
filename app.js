bc8cf901e3efb6743d4e84fb5123ed968463cbb4
John Doe <fernandesjoaopedro622@gmail.com>
2024-08-31 16:03:54 -0300

    att

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ app.get("/", (req, res) => {
 
 
 
-app.listen(3333, ()=>{
-    console.log("servidor iniciando na parte da 3333: http://localhost:3333");
-});
 
 
 
@@ -31,7 +28,7 @@ return res.json({
 
 app.post("/users", (req, res)=> {
 
-    var {name, email} = req.body;
+    var {name, email, _id, situationId} = req.body;
 
 
     return res.json({
@@ -39,3 +36,24 @@ app.post("/users", (req, res)=> {
         email: email
     });
 })
+
+app.put("/users/:id", (req, res) => {
+
+    const { id } = req.params;
+
+    const { _id, name, email, situationId } = req.body;
+
+    return res.json({ id, _id, name, email});
+});
+
+app.delete("/users/:id", (req, res) => {
+
+    const { id } = req.params;
+
+    return res.json({id});
+})
+
+
+app.listen(3333, ()=>{
+    console.log("servidor iniciando na parte da 3333: http://localhost:3333");
+});
